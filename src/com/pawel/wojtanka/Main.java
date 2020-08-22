30df600c561fdd12ff0aaf9c055afcc61a277fbd
Pawel.Wojtanka <pawel.wojtanka@networkedenergy.com>
2020-08-22 11:14:09 +0200

    simple query

--- src/com/pawel/wojtanka/Main.java
+++ src/com/pawel/wojtanka/Main.java
@@ -1,7 +1,41 @@
 package com.pawel.wojtanka;
 
-public class Main {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import java.util.TimeZone;
 
+public class Main {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/moviesrental";
+    private static final String DB_USER = "pawel";
+    private static final String DB_PASSWORD = "pawel";
+    private static final String LOCAL_TIME_ZONE = TimeZone.getDefault().getID();
+
+    public static void main(String[] args) {
+        final String query = "SELECT title, releaseDate FROM moviesinfo";
+
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", DB_USER);
+        connectionProperties.put("password", DB_PASSWORD);
+        connectionProperties.put("serverTimezone", LOCAL_TIME_ZONE);
+        try (Connection connection = DriverManager.getConnection(DB_URL, connectionProperties)) {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            System.out.println("Movies in our rental offer: ");
+            System.out.println("Title:\t|\tReleaseDate:");
+
+            while(rs.next()) {
+                System.out.println(rs.getString("title") + "\t|\t" +
+                    rs.getDate(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 }
